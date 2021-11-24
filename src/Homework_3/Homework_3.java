dadc5de549d7bd5f482ed59710c25f5fa8690ab5
Hummerhand <sh-ranger@mail.ru>
2021-11-24 20:22:40 +0600

    3 задачи решены

--- src/Homework_3/Homework_3.java
+++ src/Homework_3/Homework_3.java
@@ -1,7 +1,69 @@
 package Homework_3;
 
+import java.util.Arrays;
+
 public class Homework_3 {
     public static void main(String[] args) {
 
+        int[] array1 = {1,1,0,0,1,0};
+        returnArray(array1);
+        for (int i = 0; i < array1.length; i++) {
+            System.out.print(array1[i] + " ");
+        }
+
+        System.out.println();
+
+        int[] array2 = new int[100];
+        fillingTheArray(array2);
+        for (int i = 1; i < array2.length; i++) {
+            System.out.print(array2[i] + " ");
+        }
+
+        System.out.println();
+
+        int[] array3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        multiplicationNumbers(array3);
+        for (int i = 0; i < array3.length; i++) {
+            System.out.print(array3[i] + " ");
+        }
+    }
+
+    /*
+    1. Задать целочисленный массив, состоящий из элементов 0 и 1.
+    Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. С помощью цикла и условия заменить 0 на 1, 1 на 0;
+     */
+    public static int[] returnArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 0) {
+                array[i] = 1;
+            } else if (array[i] == 1) {
+                array[i] = 0;
+            }
+        }
+        return array;
+    }
+
+    /*
+    2. Задать пустой целочисленный массив длиной 100.
+    С помощью цикла заполнить его значениями 1 2 3 4 5 6 7 8 … 100;
+     */
+    public static int[] fillingTheArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i;
+        }
+        return array;
+    }
+
+    /*
+    3. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом,
+    и числа меньшие 6 умножить на 2;
+     */
+    public static int[] multiplicationNumbers(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < 6) {
+                array[i] = array[i] * 2;
+            }
+        }
+        return array;
     }
 }
